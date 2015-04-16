080c36a35b57daec27012209cbad799a8e1227f5
Oliver Carr <oliver.carr@gmail.com>
2015-04-16 13:10:04 +0200

    add unit test for HttpConnectionFactory

---
+++ src/test/java/eu/luminis/httpjca/HttpConnectionFactoryTest.java
@@ -0,0 +1,29 @@
+package eu.luminis.httpjca;
+
+import org.junit.Test;
+
+import javax.naming.NamingException;
+import javax.naming.Reference;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Unit test for {@link HttpConnectionFactoryImpl}.
+ */
+public class HttpConnectionFactoryTest {
+  
+  @Test
+  public void defaultConstructor() {
+    assertNotNull("default constructor null", new HttpConnectionFactoryImpl());
+  }
+
+  @Test
+  public void reference() throws NamingException {
+    HttpConnectionFactory connectionFactory = new HttpConnectionFactoryImpl();
+    Reference ref = new Reference("ref");
+    connectionFactory.setReference(ref);
+    assertEquals("reference not equal", ref, connectionFactory.getReference());
+  }
+
+}
